3b27fa638c659ea0cc000dc31132d310888f3cd6
João Pedro Sá Medeiros <jpsmedeiros@id.uff.br>
2018-08-29 08:24:31 -0300

    Criando os primeiros parsers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+#[macro_use]
+extern crate nom;
+//use nom::IResult;
+
+named!(get_greeting<&str,&str>,
+   tag_s!("hi")
+);
+
+//named!(get_greeting<&str,&str>,
+//    ws!(tag_s!("hi"))
+//);
+
+// named!(get_greeting<&str, &str>,
+//     ws!(alt!(tag_s!("hi") | tag_s!("bye")))
+// );
+
+// named!(full_greeting<&str,(&str,&str)>,
+//     pair!(
+//         get_greeting,
+//         nom::alpha
+//     )
+// );
+
+// named!(full_greeting<&str,(&str,&str)>,
+//     pair!(
+//         get_greeting,
+//         nom::alpha
+//     )
+// );
+
 fn main() {
-    println!("Hello, world!");
+    let res = get_greeting("hi there");
+    println!("{:?}", res);
+    //println!("result {:?}", full_greeting(" hi Bob  "));
+    //println!("result {:?}", full_greeting(" bye ?"));
+    // result Ok(("hi", Some("Bob")))
+    // result Ok(("bye", None))
 }
+// Done(" there", "hi")
