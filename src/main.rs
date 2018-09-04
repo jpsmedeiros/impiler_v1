cb98b2abcd3570e76e26bc6d91ab53b8c6718ee7
João Pedro Sá Medeiros <jpsmedeiros@id.uff.br>
2018-09-04 09:28:00 -0300

    Utilizando o pest

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,33 @@
+extern crate pest;
 #[macro_use]
-extern crate nom;
-//use nom::IResult;
+extern crate pest_derive;
 
-named!(get_greeting<&str,&str>,
-   tag_s!("hi")
-);
+use pest::Parser;
 
-//named!(get_greeting<&str,&str>,
-//    ws!(tag_s!("hi"))
-//);
-
-// named!(get_greeting<&str, &str>,
-//     ws!(alt!(tag_s!("hi") | tag_s!("bye")))
-// );
-
-// named!(full_greeting<&str,(&str,&str)>,
-//     pair!(
-//         get_greeting,
-//         nom::alpha
-//     )
-// );
-
-// named!(full_greeting<&str,(&str,&str)>,
-//     pair!(
-//         get_greeting,
-//         nom::alpha
-//     )
-// );
+#[derive(Parser)]
+#[grammar = "ident.pest"]
+struct IdentParser;
 
 fn main() {
-    let res = get_greeting("hi there");
-    println!("{:?}", res);
-    //println!("result {:?}", full_greeting(" hi Bob  "));
-    //println!("result {:?}", full_greeting(" bye ?"));
-    // result Ok(("hi", Some("Bob")))
-    // result Ok(("bye", None))
+    let pairs = IdentParser::parse(Rule::ident_list, "a1 b2").unwrap_or_else(|e| panic!("{}", e));
+
+    // Because ident_list is silent, the iterator will contain idents
+    for pair in pairs {
+
+        let span = pair.clone().into_span();
+        // A pair is a combination of the rule which matched and a span of input
+        println!("Rule:    {:?}", pair.as_rule());
+        println!("Span:    {:?}", span);
+        println!("Text:    {}", span.as_str());
+
+        // A pair can be converted to an iterator of the tokens which make it up:
+        for inner_pair in pair.into_inner() {
+            let inner_span = inner_pair.clone().into_span();
+            match inner_pair.as_rule() {
+                Rule::alpha => println!("Letter:  {}", inner_span.as_str()),
+                Rule::digit => println!("Digit:   {}", inner_span.as_str()),
+                _ => unreachable!()
+            };
+        }
+    }
 }
-// Done(" there", "hi")
